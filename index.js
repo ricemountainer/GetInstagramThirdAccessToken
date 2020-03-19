30396a6eb65417779d618ffa56d9baa29bafa7ac
rmblankslash <rmrmrmarmrmrm@gmail.com>
2020-03-19 16:55:47 +0900

    20200319PM1655 modified , using axios instead of request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const request = require('request');
+//const request = require('request');
+const axios = require('axios');
 const instagram_graph_api_host = 'https://graph.facebook.com/';
 
 (async()=>{
@@ -12,22 +13,46 @@ const instagram_graph_api_host = 'https://graph.facebook.com/';
         let app_id = process.argv[3];
         let app_secret = process.argv[4];
 
+        // axios client
+        let axiosClient = axios.create({
+            baseURL: instagram_graph_api_host
+        });
+
         // get second_access_token
-        let response1 = await getRequest(instagram_graph_api_host + 'oauth/access_token?grant_type=fb_exchange_token&client_id=' + app_id + '&client_secret=' + app_secret + '&fb_exchange_token=' + first_access_token);
-        let second_access_token = JSON.parse(response1.body).access_token;
+        let response1 = await axiosClient.get('oauth/access_token' , {
+            params: {
+                grant_type: 'fb_exchange_token',
+                client_id: app_id,
+                client_secret: app_secret,
+                fb_exchange_token: first_access_token
+            }
+        });
+        let second_access_token = response1.data.access_token;
 
         // get id
-        let response2 = await getRequest(instagram_graph_api_host + 'me?access_token=' + second_access_token);
-        let id = JSON.parse(response2.body).id;
+        let response2 = await axiosClient.get('me',{
+            params: {
+                access_token: second_access_token
+            }
+        });
+        let id = response2.data.id;
 
         // get third_access_token
-        let response3 = await getRequest(instagram_graph_api_host + id + '/accounts?access_token=' + second_access_token);
-        let third_access_token = JSON.parse(response3.body).data[0].access_token;
-
+        let response3 = await axiosClient.get(id+'/accounts',{
+            params: {
+                access_token: second_access_token
+            }
+        });
+        let third_access_token = response3.data.data[0].access_token;
         // get instagram business user id
         // it needs first_access_token , third_access_token can't be used it.
-        let response4 = await getRequest(instagram_graph_api_host + 'me?fields=accounts{instagram_business_account}&access_token=' + first_access_token);
-        let instagra_business_user_id = JSON.parse(response4.body).accounts.data[0].instagram_business_account.id;
+        let response4 = await axiosClient.get('me',{
+            params: {
+                fields: 'accounts{instagram_business_account}',
+                access_token: first_access_token
+            }
+        });
+        let instagra_business_user_id = response4.data.accounts.data[0].instagram_business_account.id;
 
         // stdout result
         console.log('THIRD_ACCESS_TOKEN        :' + third_access_token);
